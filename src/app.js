2aef5232902730ec3f0df03b954bea4e38042fcd
Analucia M <83628310+anilu-221@users.noreply.github.com>
2025-07-17 17:35:59 -0600

    Zip code finder functionality

--- src/app.js
+++ src/app.js
@@ -9,12 +9,10 @@ jQuery(document).ready(
 		var serviceColumns = serviceAreasFile.serviceColumns;
 		var gSuccess       = serviceAreasFile.gSuccess;
 		var gError         = serviceAreasFile.gError;
-		var sSuccess       = serviceAreasFile.sSuccess;
-		var sError         = serviceAreasFile.sError;
 		var btnText        = serviceAreasFile.btnText;
 		var btnUrl         = serviceAreasFile.btnUrl;
 		
-		/*
+		
 		// Zip Form.
 		$( '#sa-form' ).on( 'submit', function (e) {
 			e.preventDefault();
@@ -32,26 +30,106 @@ jQuery(document).ready(
 				},
 				success: function (result) {
 					//If there is an uploaded file
-					if(zipArray != false){
-						//Loop through array
-						for (let value of zipArray) {
-								//If zipcode is found
-								if(value[0] == result){
+					if( csvArray != false ){
+						// General Service.
+						if ( 'General Service' == serviceType ) {
+							for ( let value of csvArray ) {
+									//If zipcode is found
+									if( value[0] == result ){
 
-									$('#sa-result').html(`
+										$('#sa-result').html(`
 
-									<br>
-									<p style="color: #259806; font-weight: 700;">${sucessMsgEng} ${value[0]} in ${value[1] }.</p>
-									<a class="button" href="${btnUrlEng}"> ${btnTextEng} </a>
+										<br>
+										<p style="color: #259806; font-weight: 700;">${gSuccess} ${value[0]} in ${value[1] }.</p>
+										<a class="button" href="${btnUrl}"> ${btnText} </a>
 
-									`);
+										`);
 
-									break;
-								}   else{
-									//If it is not found
-									$('#sa-result').html(`<br><p> ${errorMsgEng} </p>`);
+										break;
+									}   else{
+										//If it is not found
+										$('#sa-result').html(`<br><p> ${gError} </p>`);
+									}
+							}
+						}
+
+						// Several Services.
+						if ( 'Several Services' == serviceType ) {
+							let columns  = csvArray[0];
+							let category = formSelect.val();
+							let servicesArray = [];
+							let areaName = '';
+
+							// Populate services array based on category.
+							for ( let values of csvArray ) {
+								if( values[0] == result ) {
+									areaName = values[1];
+									let index = 0;
+									for ( let item of values ) {
+										if ( item == 1) {
+											let serviceName = columns[index];
+											if ( 'all' == category ) {
+												let capService = serviceName.charAt(0).toUpperCase() + serviceName.slice(1);
+												servicesArray.push( capService );
+											} else {
+												for ( let serviceField of serviceColumns ) {
+													let serviceCategory = serviceField['service_category'];
+													if ( serviceCategory == category ) {
+														if ( serviceName == serviceField['service_title'] ) {
+															let capService = serviceName.charAt(0).toUpperCase() + serviceName.slice(1);
+															servicesArray.push( capService );
+														}
+
+													}
+													
+
+												}
+											}
+										}
+										index++;
+										
+									}
+									
 								}
 							}
+
+							// Add results to HTML.
+							// Check lenght of services Array
+							if ( servicesArray.length > 1 ) {
+								if ( 'all' == category ) {
+									$('#sa-result').html(`
+										<p style="margin:12px 0;"> <span style="color: #259806;">We offer the following services in <span style="font-weight:700;">${areaName }</span>: </span></p>
+										<ul style="margin-bottom:12px">
+										${servicesArray.map(service => `<li>${service}</li>`).join('')}
+										</ul>
+										<a class="button zip-button" href="${btnUrl}"> ${btnText} </a>
+									`);
+								} else {
+									$('#sa-result').html(`
+										<p style="margin:12px 0;"> <span style="color: #259806;">We offer the following <span style="font-weight:700;">${category }</span> services in <span style="font-weight:700;">${areaName }</span>: </span></p>
+										<ul>
+										${servicesArray.map(service => `<li>${service}</li>`).join('')}
+										</ul>
+										<a class="button zip-button" href="${btnUrl}"> ${btnText} </a>
+									`);
+								}
+							} else {
+								if ( 'all' == category ) {
+									$('#sa-result').html(`
+											<p style="margin:12px 0;">We don't offer services in ${result }.</p>
+									`);
+								} else {
+									$('#sa-result').html(`
+											<p style="margin:12px 0;">We don't offer ${category } services in ${result }.</p>
+									`);
+								}
+
+							}
+
+	
+							
+
+						}
 					}
 					else{
 						$('#sa-result').html('ERROR: upload csv file with zip codes');
@@ -60,6 +138,5 @@ jQuery(document).ready(
 				}
 			});
 		});
-		*/
 	}
 );
